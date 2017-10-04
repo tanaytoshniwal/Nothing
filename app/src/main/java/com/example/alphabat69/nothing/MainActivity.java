f2dcb49b10cd87df9756509407722296a8460d6d
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-10-04 10:43:36 +0530

    This is an app that does NOTHING

--- app/src/main/java/com/example/alphabat69/nothing/MainActivity.java
+++ app/src/main/java/com/example/alphabat69/nothing/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.alphabat69.nothing;
 
+import android.content.Intent;
 import android.os.Build;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ConstraintLayout layout;
+    Bundle temp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT<16) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
+        temp=savedInstanceState;
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        layout=(ConstraintLayout)findViewById(R.id.layout);
+    }
+
+    @Override
+    public void onBackPressed() {}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Intent myIntent=new Intent(this,MainActivity.class);
+        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        myIntent.setAction(Intent.ACTION_MAIN);
+        startActivity(myIntent);
+        finish();
     }
 }
