82e1b1c9a95c4f1ed602516c416f5dc22dfd3d19
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-24 19:14:01 +0530

    completed

--- app/src/main/java/com/example/alphabat69/nothing/MainActivity.java
+++ app/src/main/java/com/example/alphabat69/nothing/MainActivity.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
     @Override
