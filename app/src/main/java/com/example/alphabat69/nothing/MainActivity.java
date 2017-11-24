1816ccfacc93bcab0dfdf741e2e8437321551bb7
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-24 18:25:02 +0530

    improvement

--- app/src/main/java/com/example/alphabat69/nothing/MainActivity.java
+++ app/src/main/java/com/example/alphabat69/nothing/MainActivity.java
@@ -1,66 +1,21 @@
 package com.example.alphabat69.nothing;
 
-import android.app.ActivityManager;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.WindowManager;
-import android.widget.Toast;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ConstraintLayout layout;
-    Bundle temp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        temp=savedInstanceState;
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        layout=(ConstraintLayout)findViewById(R.id.layout);
 
     }
 
     @Override
-    public void onBackPressed() {}
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        switch(keyCode){
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
-            case KeyEvent.KEYCODE_VOLUME_UP:
-            case KeyEvent.KEYCODE_HOME:
-            case KeyEvent.KEYCODE_BACK:{
-                Intent myIntent=new Intent(this,MainActivity.class);
-                myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                myIntent.setAction(Intent.ACTION_MAIN);
-                startActivity(myIntent);
-            }
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Intent myIntent=new Intent(this,MainActivity.class);
-        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        myIntent.setAction(Intent.ACTION_MAIN);
-        startActivity(myIntent);
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        Intent myIntent=new Intent(this,MainActivity.class);
-        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        myIntent.setAction(Intent.ACTION_MAIN);
-        startActivity(myIntent);
+    public void onBackPressed() {
+        //nothing
     }
 }
